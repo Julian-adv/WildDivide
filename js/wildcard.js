e47d1b75989440fd57e8759a92ee40d9ea880f2d
Julian <julian.adventerer.dev@gmail.com>
2024-10-13 13:01:05 +0900

    Fix refresh button

--- js/wildcard.js
+++ js/wildcard.js
@@ -1,18 +1,24 @@
 import { ComfyApp, app } from "../../scripts/app.js";
 import { api } from "../../scripts/api.js";
 
-let refresh_btn = document.getElementById("comfy-refresh-button");
-let refresh_btn2 = document.querySelector(
-  'button[title="Refresh widgets in nodes to find new models or files"]'
-);
+// let refresh_btn = document.getElementById("comfy-refresh-button");
+// let refresh_btn2 = document.querySelector(
+//   'button[title="Refresh widgets in nodes to find new models or files"]'
+// );
+let refresh_btn3 = document.querySelector("#pv_id_9_content > div > div > span.p-buttongroup.p-component.flex.flex-nowrap > button")
 
-let orig = refresh_btn.onclick;
+// let orig = refresh_btn.onclick;
 
-refresh_btn.onclick = function () {
-  orig();
-  api.fetchApi("/wilddivide/refresh");
-};
+// refresh_btn.onclick = function () {
+//   orig();
+//   api.fetchApi("/wilddivide/refresh");
+// };
 
-refresh_btn2.addEventListener("click", function () {
+// refresh_btn2.addEventListener("click", function () {
+//   api.fetchApi("/wilddivide/refresh");
+// });
+
+refresh_btn3.addEventListener("click", function () {
   api.fetchApi("/wilddivide/refresh");
 });
+
