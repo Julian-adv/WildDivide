81014085c0272b0198761d92894be22d8e12461f
Julian <julian.adventerer.dev@gmail.com>
2024-11-27 23:24:31 +0900

    Add delete value button
    - Change icon to svg

--- js/wild_prompt_generator.js
+++ js/wild_prompt_generator.js
@@ -225,11 +225,12 @@ function setup_node(node) {
             backgroundColor: "transparent",
             border: "none",
             cursor: "pointer",
-            padding: "0px",
-            // height: "fit-content",
-            transform: "rotateZ(90deg)",
+            padding: "2px 0px",
+            width: "16px",
+            height: "16px",
+            color: "var(--p-form-field-float-label-color)",
         });
-        button.textContent = "✎";
+        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"> <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" /> </svg>';
         button.onclick = () => {
             setup_and_show_edit_dialog(widgetName);
         };
@@ -429,7 +430,7 @@ function show_dialog_internal(dialog, title, slotNameElement, values) {
     const container = document.createElement("div");
     Object.assign(container.style, {
         display: "grid",
-        gridTemplateColumns: "auto 1fr",
+        gridTemplateColumns: "auto 1fr auto",
         alignItems: "baseline",
         gap: "4px",
         marginTop: "10px",
@@ -441,7 +442,7 @@ function show_dialog_internal(dialog, title, slotNameElement, values) {
     });
     slotLabel.textContent = "Slot  ";
 
-    container.append(slotLabel, slotNameElement);
+    container.append(slotLabel, slotNameElement, document.createElement("span"));
 
     const marker = document.createElement("span");
     const addButton = document.createElement("button");
@@ -497,6 +498,24 @@ function add_new_value(label, value, marker) {
     };
     valueElement.addEventListener("input", adjustHeight);
     setTimeout(adjustHeight, 0);
-    marker.before(labelElement, valueElement);
+
+    const deleteButton = document.createElement("button");
+    Object.assign(deleteButton.style, {
+        fontSize: "14px",
+        backgroundColor: "transparent",
+        padding: "0px",
+        border: "none",
+        cursor: "pointer",
+        width: "16px",
+        height: "16px"
+    });
+    deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"> <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /> </svg>';
+    deleteButton.onclick = function() {
+        valueElements.splice(valueElements.indexOf(valueElement), 1);
+        labelElement.remove();
+        valueElement.remove();
+        deleteButton.remove();
+    }
+    marker.before(labelElement, valueElement, deleteButton);
     return valueElement;
 }
